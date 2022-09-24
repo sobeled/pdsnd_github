98c4e3194882ae1f161caf17931bc0d537118f96
Evan Sobel <sobeled@gmail.com>
2022-09-23 21:46:12 -0400

    Added comment describing the csv data files

--- bikeshare_sobel_R1.py
+++ bikeshare_sobel_R1.py
@@ -4,6 +4,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# These data files are a limited view of the original data in order to streamline exploration of the data.
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
